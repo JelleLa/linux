38ce62301c2ec828db44b7a9d63b3e344d0da1b4
Jelle Langedijk <60575963+JelleLa@users.noreply.github.com>
2020-07-03 13:13:00 +0200

    Add files via upload

---
+++ INSTALL_SOLUS.sh
@@ -0,0 +1,147 @@
+## THIS SCRIPT INSTALLS ALL MY NECESSARY SOFTWARE TO UBUNTU LINUX ##
+## (c) JELLE LANGEDIJK 2020 ##
+## TO RUN: sudo sh a.sh ## 
+
+
+echo "
+___     _____         __ _ _ ___ ____  __ _       __
+ | |\ |(_  | /\ | |  (_ / |_) | |_)|  (_ / \| | |(_ 
+_|_| \|__) |/--\|_|_ __)\_| \_|_|  |  __)\_/|_|_|__)
+"
+
+echo "(c) 2020 Jelle Langedijk, all rights reserved"
+
+
+echo
+echo 
+## ENSURE SYSTEM IS UP TO DATE ##
+echo "CHECKING FOR UPDATES" &&
+/bin/sleep 5
+sudo eopkg upgrade &&
+/bin/sleep 1
+
+
+echo
+echo 
+## INSTALL NEOFETCH ##
+echo "INSTALLING NEOFETCH" &&
+/bin/sleep 5
+sudo eopkg it neofetch &&
+/bin/sleep 1
+
+## INSTALL GIT ##
+echo "INSTALLING GIT" &&
+/bin/sleep 5
+sudo eopkg it git
+/bin/sleep 1
+
+
+echo
+echo
+## VIM SETUP ##
+
+read -p "INSTALL VIM? (enter y (yes) or n (no))" la
+case "$la" in
+    y)  echo; echo; sudo eopkg it vim ;;
+    n)  echo; echo; echo SKIPPED VIM INSTALLATION ;;
+    *)  echo; echo; echo "enter y (yes) or n (no)" ;;
+esac
+
+/bin/sleep 1
+
+
+echo
+echo
+## LATEX SETUP ##
+
+read -p "INSTALL TeXLive ? (enter y (yes) or n (no))" la
+case "$la" in
+    y)  echo; echo; sudo eopkg it texlive ;;
+    n)  echo; echo; echo SKIPPED TeXLive INSTALLATION ;;
+    *)  echo; echo; echo "enter y (yes) or n (no)" ;;
+esac
+
+/bin/sleep 1
+
+
+echo
+echo
+## LOLLYPOP SETUP ##
+
+read -p "INSTALL LOLLYPOP MUSIC PLAYER ? (enter y (yes) or n (no))" lo
+case "$lo" in
+    y)  echo; echo; sudo eopkg it lollypop ;;
+    n)  echo; echo; echo SKIPPED LOLLYPOP MUSIC PLAYER INSTALLATION ;;
+    *)  echo; echo; echo "enter y (yes) or n (no)" ;;
+esac
+
+/bin/sleep 1
+
+echo
+echo
+## OBS-Studio SETUP ##
+
+read -p "INSTALL OBS STUDIO ? (enter y (yes) or n (no))" ob
+case "$ob" in
+    y)  echo; echo; sudo eopkg it obs-studio ;;
+    n)  echo; echo; echo SKIPPED OBS STUDIO INSTALLATION ;;
+    *)  echo; echo; echo "enter y (yes) or n (no)" ;;
+esac
+
+/bin/sleep 1
+
+
+echo ALL SOFTWARE INSTALLED!
+echo
+/bin/sleep 2
+
+read -p "REBOOT NOW?. (enter y (yes) or n (no))" re
+case "$re" in
+    y)  echo; echo; reboot ;;
+    n)  echo; echo; echo  ;;
+    *)  echo; echo; echo "enter y (yes) or n (no)" ;;
+esac
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
